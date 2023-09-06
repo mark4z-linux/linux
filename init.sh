4aa90fc9fa3b1902c3380451fd6731d8ce9aea52
Mengchao.Lv1 <Mengchao.Lv1@>
2023-09-06 14:04:59 +0800

    init

--- init.sh
+++ init.sh
@@ -1,11 +1,10 @@
+#https://risc-v-getting-started-guide.readthedocs.io/en/latest/linux-qemu.html
+#https://github.com/riscv-collab/riscv-gnu-toolchain
+#https://github.com/mirror/busybox.git
+
 apt update
-apt-get install -y ncurses-dev
-apt-get install -y flex
-apt-get install -y bison
-apt-get install -y libelf-dev
-apt-get install -y libssl-dev
-apt install -y dwarves
-apt-get install -y zstd
-apt install -y bear
-apt install -y bear
-apt install -y bc
+apt install -y autoconf automake autotools-dev curl libmpc-dev libmpfr-dev libgmp-dev \
+                 gawk build-essential bison flex texinfo gperf libtool patchutils bc \
+                 zlib1g-dev libexpat-dev git bear libpixman-1-dev
+
+apt-get install -y git build-essential gdb-multiarch qemu-system-misc gcc-riscv64-linux-gnu binutils-riscv64-linux-gnu ninja-build
