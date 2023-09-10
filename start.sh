d7c37a27d32ddbe9f1432cbf5cde3a5843a90297
mark4z <mark4z@apache.org>
2023-09-10 18:30:04 +0800

    start

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 qemu-system-riscv64 -nographic -machine virt \
-     -kernel arch/riscv/boot/Image -append "root=/dev/vda rw console=ttyS0" \
+     -kernel arch/riscv/boot/Image -append "root=/dev/vda rw console=ttyS0,nokaslr" \
      -drive file=../busybox/rootfs.img,format=raw,id=hd0 \
      -device virtio-blk-device,drive=hd0 -s -S
